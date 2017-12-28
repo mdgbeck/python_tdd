18e06850b705ad5726d7076b9c6a3272ba97a7f6
Michael Groesbeck <mdgbeck@gmail.com>
2017-12-28 01:58:43 -0500

    functional tests check to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,17 +17,32 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         # user sees to do in title
-        self.assertIn("To-Do", self.browser.title)
-        self.fail('Finish the test!')
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # user is invited to add to do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # user types 'item 1' into text box
+        inputbox.send_keys('item 1')
 
         # user hits enter and sees item 1 on list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: item 1' for row in rows)
+        )
 
         # user continues to see textbox inviting to add more. Enters 'item 2'
-
+        self.fail('finish the tests')
         # user sees both items on superlists
 
         # user notices site displays unique url for her
