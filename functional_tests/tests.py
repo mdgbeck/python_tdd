74dad1f80c2f57b08b13735c735e8f4c1b73b305
Michael Groesbeck <mdgbeck@gmail.com>
2018-01-16 14:20:37 -0500

    end chapter 6

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,10 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import  WebDriverException
 import time
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -15,6 +18,19 @@ class NewVisitorTest(LiveServerTestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise # -*- coding: utf-8 -*-
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
 
@@ -38,18 +54,16 @@ class NewVisitorTest(LiveServerTestCase):
 
         # user hits enter and sees item 1 on list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: item 1')
+        self.wait_for_row_in_list_table('1: item 1')
 
         # user continues to see textbox inviting to add more. Enters 'item 2'
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('item 2')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # user sees both items on superlists
-        self.check_for_row_in_list_table('1: item 1')
-        self.check_for_row_in_list_table('2: item 2')
+        self.wait_for_row_in_list_table('1: item 1')
+        self.wait_for_row_in_list_table('2: item 2')
 
         # user notices site displays unique url for her
 
