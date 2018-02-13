3bbb10c3636b992ebcec0c6af6b68a06a7e939de
Michael Groesbeck <mdgbeck@gmail.com>
2018-02-13 10:46:54 -0500

    hack allowed_hosts

--- superlists/settings.py
+++ superlists/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dp*1r__2h!p6aus%u%e@iaur@l6=#^t*jxj+z(st948qh9&4n-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
