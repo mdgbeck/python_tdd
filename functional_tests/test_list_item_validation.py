df84feac9696cdbeefa516b74e7ea0af9e1fd505
Michael Groesbeck <mdgbeck@gmail.com>
2018-02-21 05:41:19 -0500

    use form in all views, working end 14

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -11,13 +11,17 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys(Keys.ENTER)
 
         # page refreshes, gives error saying list item cannot be blank
-        self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
-            "You can't have an empty list item"
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+            '#id_text:invalid'
         ))
 
-        # tries again with text in field, works
+        # tries again with text in field and error disapperats
         self.get_item_input_box().send_keys('item 1')
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+            '#id_text:valid'
+        ))
+
+        # can submit
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: item 1')
 
@@ -25,15 +29,16 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys(Keys.ENTER)
 
         # receives a similar warning
-        self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
-            "You can't have an empty list item"
+        self.wait_for_row_in_list_table('1: item 1')
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+            '#id_text:invalid'
         ))
 
         # she can correct by filling in some text
         self.get_item_input_box().send_keys('not blank')
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+            '#id_text:valid'
+        ))
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: item 1')
         self.wait_for_row_in_list_table('2: not blank')
-
-        self.fail('finish this test')
