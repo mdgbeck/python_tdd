b7404822d9690f97e1f3f803021c58579c60b642
Michael Groesbeck <mdgbeck@gmail.com>
2018-04-03 10:23:17 -0400

    begin to set up server pop email, need to set up email still

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -6,19 +6,28 @@ import time
 
 MAX_WAIT = 10
 
+
+def wait(fn):
+    def modified_fn(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+    return modified_fn
+
+
 class FunctionalTest(StaticLiveServerTestCase):
 
-    def wait(fn):
-        def modified_fn(*args, **kwargs):
-            start_time = time.time()
-            while True:
-                try:
-                    return fn(*args, **kwargs)
-                except (AssertionError, WebDriverException) as e:
-                    if time.time() - start_time > MAX_WAIT:
-                        raise e
-                    time.sleep(0.5)
-        return modified_fn
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.staging_server = os.environ.get('STAGING_SERVER')
+        if self.staging_server:
+            self.live_server_url = 'http://' + self.staging_server
+
 
     @wait
     def wait_to_be_logged_in(self, email):
